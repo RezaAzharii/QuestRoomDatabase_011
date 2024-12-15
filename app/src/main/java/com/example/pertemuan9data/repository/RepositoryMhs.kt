aa9f3b2ef6cdd2ef3a760c57a01225b29a74cf84
RezaAzharii <rezaazhari867@gmail.com>
2024-12-15 22:38:16 +0700

    Memanggil semua fungsi yang sudah dideklarasikan pada MahasiswaDao untuk meengabil data

--- app/src/main/java/com/example/pertemuan9data/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan9data/repository/RepositoryMhs.kt
@@ -1,7 +1,14 @@
 package com.example.pertemuan9data.repository
 
 import com.example.pertemuan9data.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    //Fungsi memanggil data dari fungsi getMahasiswa dari Dao
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getMhs(nim: String): Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
