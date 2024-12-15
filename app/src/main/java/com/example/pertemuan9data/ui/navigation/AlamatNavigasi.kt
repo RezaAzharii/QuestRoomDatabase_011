280af49dfe16eb98593540f89b52949262391597
RezaAzharii <rezaazhari867@gmail.com>
2024-12-15 22:42:40 +0700

    Mendefinisikan route yang akan digunakan dalam navigasi

--- app/src/main/java/com/example/pertemuan9data/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan9data/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,17 @@ package com.example.pertemuan9data.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi{
+    override val route = "home"
+}
+object DestinasiDetail : AlamatNavigasi{
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+object DestinasiUpdate : AlamatNavigasi{
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "${DestinasiDetail.route}/{$NIM}"
+}
