0374e64e4b838bce199480cf074a9bcfcbaf7497
RezaAzharii <rezaazhari867@gmail.com>
2024-12-15 22:29:54 +0700

    Menambahkan fungsi untuk mengambil semua data pada tabel mahasiswa secara berurutan berdasarkan nama dari A-Z

--- app/src/main/java/com/example/pertemuan9data/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9data/data/dao/MahasiswaDao.kt
@@ -1,11 +1,20 @@
 package com.example.pertemuan9data.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.pertemuan9data.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    //Fungsi mengambil semua data pada tabel Mahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
 }
